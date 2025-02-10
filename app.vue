2a4f79e25decdeb1e46097c5918c00897a0471b9
Jan Walenda <log@janwalenda.de>
2025-02-10 17:06:04 +0100

    refactor(start): :sparkles: Init nuxt project

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup>
+  useSeoMeta({
+    title: 'Jan Walenda',
+    description: 'Jan Walenda / Frontend Developer',
+    charset: "utf-8",
+  });
+</script>
+
+<template>
+  <main id="app">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </main>
+</template>
