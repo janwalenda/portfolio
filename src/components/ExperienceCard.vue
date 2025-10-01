998d3cf3c3ca89a9fc154870bbdd055a1d848a4e
Jan Mac <log@janwalenda.de>
2025-10-01 13:41:30 +0200

    Refactor index page to use ExperienceList component

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  technology: string,
+  skillLevel: number,
+  company: string,
+  role: string,
+}>()
+</script>
+<template>
+  <details class="border border-base-300 bg-base-100 rounded-box w-full">
+    <summary class="p-2 grid grid-cols-[.5fr_1fr] col-start-auto gap-2.5 items-center cursor-pointer hover:bg-base-300">
+      <Icon :name="`logos:${props.technology}`"/>
+      <progress class="progress" :max="100" :value="props.skillLevel"/>
+    </summary>
+    <div class="p-2">
+      <p>
+        <span>{{ props.company }}</span>
+        <span>{{ props.role }}</span>
+      </p>
+    </div>
+  </details>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,85 +1,8 @@
-<script lang="ts" setup>
-import {
-  utils,
-  onScroll,
-  animate,
-} from 'animejs';
-const { t } = useI18n();
-
-function getCSSVar(varName: string) {
-  return window.getComputedStyle(document.documentElement).getPropertyValue(varName);
-}
-
-onMounted(() => {
-  const container = document.getElementById('sticky-container');
-  utils.$('.card').forEach($square => {
-    console.log(getCSSVar('--color-base-300'))
-    animate($square, {
-      x: `${container?.getBoundingClientRect().width as number - $square.getBoundingClientRect().width}px`,
-      backgroundColor: [getCSSVar('--color-base-300'), getCSSVar('--color-base-100')],
-      boxShadow: '10px 10px 0px 2px rgba(0,0,0,0.75)',
-      duration: 2000,
-      alternate: true,
-      ease: 'inOut',
-      autoplay: onScroll({
-        container: document.body,
-        sync: 1,
-        enter: 'max bottom',
-        leave: 'min top',
-      }),
-    });
-  });
-})
-
-</script>
 <template>
-  <div id="sticky-container" class="flex flex-1 flex-col gap-20 w-full">
-    <span class="hidden shadow-xl" />
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-    <div class="card p-4 border-2 border-base-300 w-fit text-neutral-700">{{ t('home.title') }}</div>
-  </div>
+  <ExperienceList />
 </template>
 <script lang="ts">
 export default {
-  name: 'IndexPage',
-}
+  name: "IndexPage",
+};
 </script>
-<i18n lang="json">{
-  "de": {
-    "home.title": "Startseite"
-  },
-  "en": {
-    "home.title": "Homepage"
-  },
-  "ja": {
-    "home.title": "ホーム"
-  }
-}</i18n>
