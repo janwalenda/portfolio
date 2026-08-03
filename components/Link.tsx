0d195ceebd2ff27a095e159a83c5d961979e94c4
Jan Walenda <log@janwalenda.de>
2026-08-03 15:07:22 +0200

    refactor: Simplify Link component by removing unused LinkProps and updating icon rendering logic.

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,15 +1,12 @@
 import type { Link as LinkType } from "@/sanity.types";
 import { Icon } from "@iconify/react";
-import type { LinkProps } from "next/link";
 import NextLink from "next/link";
 
-export default function Link({ title, icon, slug, url }: LinkProps & LinkType) {
+export default function Link({ title, icon, slug, url }: LinkType) {
   return (
-    <NextLink href={url ? url : slug?.current || ''}>
-      {icon ?? (
-        <Icon icon={icon} />
-      )}
+    <NextLink href={url ? url : slug?.current || ""}>
+      {icon?.name && <Icon icon={icon.name} />}
       {title}
     </NextLink>
-  )
+  );
 }
